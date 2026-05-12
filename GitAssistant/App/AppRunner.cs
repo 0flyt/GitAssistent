fb2df0267e3d9b750081b90f40b249bfefec02c1
0flyt <christopher.rydberg@gmail.com>
2026-05-12 19:23:28 +0200

    Add AppRunner with Welcome text

---
+++ GitAssistant/App/AppRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitAssistant.App;
+
+public class AppRunner
+{
+
+    public void Run()
+    {
+        Console.WriteLine("GitASS v.0.0.0001");
+        Console.WriteLine("Beep boop");
+    }
+}
